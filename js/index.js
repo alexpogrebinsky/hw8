b78833402dbd12b851a16053cae59fb26935a610
Alexander A Pogrebinsky <alex@pogrebinskygroup.com>
2018-02-02 10:25:31 -0500

    Update index.js
    updated the index.js to get rid of global variables and make scope variables

--- js/index.js
+++ js/index.js
@@ -1,93 +1,83 @@
-var x = Math.floor((Math.random() * 10) + 1);
-var y = Math.floor((Math.random() * 10) + 1);            
-var z = Math.floor((Math.random() * 10) + 1);
+function ProcessBox1() {
+    var ranNum;
 
+    do {
+        ranNum = Math.floor((Math.random() * 10) + 1);
+    } while (ranNum == 0 || ranNum >= 4)
 
-function myFunction() {
-            
-
-            if (x == 1) {
-                document.getElementById("one").className ="one";
+    if (ranNum == 1) {
+        document.getElementById("one").className = "one";
     }
-            else if (x == 2) {
-                document.getElementById("one").className = "two";
-    }
-
-            else if (x == 3) {
-                document.getElementById("one").className = "three";
-
+    else if (ranNum == 2) {
+        document.getElementById("one").className = "two";
     }
 
+    else if (ranNum == 3) {
+        document.getElementById("one").className = "three";
+    }
+    return ranNum;
 }
 
 
-function myFunctionTwo() {
-            
+function ProcessBox2() {
+    do {
+        ranNum2 = Math.floor((Math.random() * 10) + 1);
+    } while (ranNum2 == 0 || ranNum2 >= 4)
 
+    if (ranNum2 == 1) {
+        document.getElementById("second").className = "one";
+    }
+    else if (ranNum2 == 2) {
+        document.getElementById("second").className = "two";
+    }
 
-            if (y == 1) {
-                document.getElementById("second").className = "one";
-        }
-            else if (y == 2) {
-                document.getElementById("second").className = "two";
-        }
+    else if (ranNum2 == 3) {
+        document.getElementById("second").className = "three";
+    }
 
-            else if (y == 3) {
-                document.getElementById("second").className = "three";
-
-            }
-            
+    return ranNum2;
 }
 
 
 
- function myFunctionThree() {
-            
-  
+function ProcessBox3() {
+    do {
+        ranNum3 = Math.floor((Math.random() * 10) + 1);
+    } while (ranNum3 == 0 || ranNum3 >= 4)
+    if (ranNum3 === 1) {
+        document.getElementById("third").className = "one";
+    }
+    else if (ranNum3 === 2) {
+        document.getElementById("third").className = "two";
+    }
+    else if (ranNum3 === 3) {
+        document.getElementById("third").className = "three";
+    }
+    return ranNum3;
+}
 
-
-            if (z === 1) {
-                document.getElementById("third").className = "one";
-        }
-            else if (z === 2) {
-                document.getElementById("third").className = "two";
-        }
-            else if (z === 3) {
-                document.getElementById("third").className = "three";
-
-        }
-  
-};
-
-function Congrats() {
-
-
-    if (x === 1 && y === 1 && z === 1) {
+function Congrats(ranNum, ranNum2, ranNum3) {
+    if (ranNum === 1 && ranNum2 === 1 && ranNum3 === 1) {
         document.getElementById('msg').innerHTML = 'Congrats! You Won!';
     }
-    else if (x === 2 && y === 2 & z === 2) {
-        document.getElementById('msg').innerHTML = "Congrats! You Won!";
+    else if (ranNum === 2 && ranNum2 === 2 & ranNum3 === 2) {
+    document.getElementById('msg').innerHTML = "Congrats! You Won!";
     }
-    else if (x === 3 && y === 3 && z === 3) {
+   else if (ranNum === 3 && ranNum2 === 3 && ranNum3 === 3) {
         document.getElementById('msg').innerHTML = "Congrats! You Won!";
-    }
-
+   }
     else {
         document.getElementById('msg').innerHTML = "Sorry! Try Again!";
     }
-    
-    
 }
-
-
-
 
 
 function startProcess() {
+    var ranNum = ProcessBox1();
+    var ranNum2 = ProcessBox2();
+    var ranNum3 = ProcessBox3();
 
-    myFunction();
-    myFunctionTwo();
-    myFunctionThree();
-    Congrats();
-    
+    Congrats(ranNum, ranNum2, ranNum3);
 }
+
+startProcess();
